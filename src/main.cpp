aa13b94b8cb1d17cdfaf93e41cab565e490c7340
Jack <asecta@hotmail.com>
2019-10-25 15:15:14 +0100

    Code cleanup and some comments .

--- src/main.cpp
+++ src/main.cpp
@@ -6,41 +6,42 @@
 
 using namespace std;
 
-const int scale = 2;
-const int width = 512;
-const int height = 512;
-const int boardSize = width * height;
+const int GUI_SCALE = 2;
+const int WIDTH = 512;
+const int HEIGHT = 512;
+const int BOARD_SIZE = WIDTH * HEIGHT;
+
+const int BLACK = 0x0;
+const int WHITE = 0xFFFFFF;
 
 const int MODE_SERIAL = 0;
 const int MODE_PARALLEL = 1;
 const int MODE_PARALLEL_BATCHED = 4;
 
 const int BATCHED_THREAD_COUNT = 4;
-const int BATCHED_THREAD_STRIDE = boardSize / BATCHED_THREAD_COUNT;
+const int BATCHED_THREAD_STRIDE = BOARD_SIZE / BATCHED_THREAD_COUNT;
 
 const int CURRENT_MODE = MODE_PARALLEL_BATCHED;
 
-
-const int BLACK = 0x0;
-const int WHITE = 0xFFFFFF;
-
-vector<int> gameBoard(boardSize, WHITE);
+// Storing the game as integers rather than booleans, as it's easier to render and i think c++ stores bools as 16 bits anyway.
+vector<int> currentGameState(BOARD_SIZE, WHITE);
 
 bool getCellByIndex(vector<int> &board, int index) {
-    if (index < 0 || index >= boardSize) return false;
+    if (index < 0 || index >= BOARD_SIZE) return false;
     return board[index] == BLACK;
 }
 
 int countAliveNeighbours(vector<int> &board, int index) {
     int count = 0;
-    count += getCellByIndex(board, index - width - 1) ? 1 : 0;
-    count += getCellByIndex(board, index - width) ? 1 : 0;
-    count += getCellByIndex(board, index - width + 1) ? 1 : 0;
+    // Hard-Coded neighbour iteration since idk how c++ unwraps for loops and it's easier on a 1d array.
+    count += getCellByIndex(board, index - WIDTH - 1) ? 1 : 0;
+    count += getCellByIndex(board, index - WIDTH) ? 1 : 0;
+    count += getCellByIndex(board, index - WIDTH + 1) ? 1 : 0;
     count += getCellByIndex(board, index - 1) ? 1 : 0;
     count += getCellByIndex(board, index + 1) ? 1 : 0;
-    count += getCellByIndex(board, index + width - 1) ? 1 : 0;
-    count += getCellByIndex(board, index + width) ? 1 : 0;
-    count += getCellByIndex(board, index + width + 1) ? 1 : 0;
+    count += getCellByIndex(board, index + WIDTH - 1) ? 1 : 0;
+    count += getCellByIndex(board, index + WIDTH) ? 1 : 0;
+    count += getCellByIndex(board, index + WIDTH + 1) ? 1 : 0;
     return count;
 }
 
@@ -54,44 +55,43 @@ bool updateCell(vector<int> &board, int index) {
     return state;
 }
 
-void serialUpdate(vector<int> &oldGameBoard, vector<int> &newGameBoard) {
-    for (int index = 0; index < boardSize; index++) {
-        newGameBoard[index] = updateCell(oldGameBoard, index) ? BLACK : WHITE;
+void serialUpdate(vector<int> &oldGameState, vector<int> &newGameState) {
+    for (int index = 0; index < BOARD_SIZE; index++) {
+        newGameState[index] = updateCell(oldGameState, index) ? BLACK : WHITE;
     }
 }
 
-void parallelUpdate(vector<int> &oldGameBoard, vector<int> &newGameBoard) {
-    tbb::parallel_for(size_t(0), size_t(oldGameBoard.size()), [&](size_t index) {
-        newGameBoard[index] = updateCell(oldGameBoard, index) ? BLACK : WHITE;
+void parallelUpdate(vector<int> &oldGameState, vector<int> &newGameState) {
+    tbb::parallel_for(size_t(0), size_t(oldGameState.size()), [&](size_t index) {
+        newGameState[index] = updateCell(oldGameState, index) ? BLACK : WHITE;
     });
 }
 
-void batchedUpdate(vector<int> &oldGameBoard, vector<int> &newGameBoard) {
+void batchedUpdate(vector<int> &oldGameState, vector<int> &newGameState) {
     tbb::parallel_for(0, BATCHED_THREAD_COUNT, [&](size_t threadIndex) {
-        for (int batchIndex = threadIndex * BATCHED_THREAD_STRIDE;
-             batchIndex < (threadIndex + 1) * BATCHED_THREAD_STRIDE; batchIndex++) {
-            int index = +batchIndex;
-            newGameBoard[index] = updateCell(oldGameBoard, index) ? BLACK : WHITE;
+        for (int index = threadIndex * BATCHED_THREAD_STRIDE;
+             index < (threadIndex + 1) * BATCHED_THREAD_STRIDE; index++) {
+            newGameState[index] = updateCell(oldGameState, index) ? BLACK : WHITE;
         }
     });
 }
 
 void updateGame() {
-    vector<int> newGameBoard(boardSize, WHITE);
+    vector<int> newGameState(BOARD_SIZE, WHITE);
 
     switch (CURRENT_MODE) {
         case MODE_SERIAL:
-            serialUpdate(gameBoard, newGameBoard);
+            serialUpdate(currentGameState, newGameState);
             break;
         case MODE_PARALLEL:
-            parallelUpdate(gameBoard, newGameBoard);
+            parallelUpdate(currentGameState, newGameState);
             break;
         case MODE_PARALLEL_BATCHED:
-            batchedUpdate(gameBoard, newGameBoard);
+            batchedUpdate(currentGameState, newGameState);
             break;
     }
 
-    gameBoard = newGameBoard;
+    currentGameState = newGameState;
 }
 
 
@@ -109,49 +109,43 @@ void populate() {
     for (int i = 0; i < vectorArray.size(); i += 2) {
         int cellX = vectorArray[i];
         int cellY = vectorArray[i + 1];
-        gameBoard[cellY * height + cellX] = BLACK;
+        currentGameState[cellY * HEIGHT + cellX] = BLACK;
     }
-    
+
     initialStateFile.close();
 }
 
 int main(int argc, char *argv[]) {
-    using std::cerr;
-    using std::endl;
-
     // the following is pretty much SDL2 boilerplate
-
-    SDL_Window *win = SDL_CreateWindow("Game of life", 100, 100, width * scale, height * scale, SDL_WINDOW_SHOWN);
-    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_Window *window = SDL_CreateWindow("Game of life", 100, 100, WIDTH * GUI_SCALE, HEIGHT * GUI_SCALE, SDL_WINDOW_SHOWN);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "0");
-    SDL_Texture *tex = SDL_CreateTexture(ren, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING, width, height);
 
     populate();
 
     bool quit = false;
-    SDL_Event e;
+    SDL_Event event;
 
     while (!quit) {
         // Poll the even loop, otherwise windows times out the application
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) {
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
                 quit = true;
             }
         }
 
         updateGame();
-
         // Draw our game.
-        SDL_UpdateTexture(tex, nullptr, gameBoard.data(), width * sizeof(int));
-        SDL_RenderClear(ren);
-        SDL_RenderCopy(ren, tex, nullptr, nullptr);
-        SDL_RenderPresent(ren);
+        SDL_UpdateTexture(texture, nullptr, currentGameState.data(), WIDTH * sizeof(int));
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+        SDL_RenderPresent(renderer);
     }
 
-    SDL_DestroyTexture(tex);
-    SDL_DestroyRenderer(ren);
-    SDL_DestroyWindow(win);
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
-
     return EXIT_SUCCESS;
 }
